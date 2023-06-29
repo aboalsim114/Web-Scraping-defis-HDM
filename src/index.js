47b0faf1531392cbf85ad55ba9fc72370d4be988
aboalsim114 <aboalsim114@gmail.com>
2023-06-29 18:07:41 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-const puppeteer = require("puppeteer");
-const fs = require("fs/promises");
+const puppeteer = require('puppeteer');
+const fs = require('fs/promises');
+
+async function startBrowser() {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    return { browser, page };
+}
+
+async function goToURL(page, url) {
+    await page.goto(url);
+    await page.waitForNetworkIdle();
+}
+
+async function captureScreenshot(page, path) {
+    await page.screenshot({ path, fullPage: true });
+}
+
+async function extractNames(page) {
+    return await page.evaluate(() => {
+        return Array.from(document.querySelectorAll("#component_53 > div > div.acKDw.w.O > h1")).map(x => x.textContent);
+    });
+}
+
+async function writeToFile(path, data) {
+    await fs.writeFile(path, data.join("\n"));
+}
 
 async function start() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("https://fr.tripadvisor.be/FindRestaurants?geo=188646&diningOptions=10600&establishmentTypes=10591&broadened=true");
-    await page.screenshot({ path: "images/bg.png", fullPage: true });
-    const names = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll("#lithium-root > main > div > div:nth-child(5) > div.sVSQW > div > div.seiBA._T.wCxWg > div.LvzMf._T > div.Ikpld.f.e > span:nth-child(1) > div > div > div.yJIls.z.P0.M0 > header > div > div.jhsNf.N.G > div.VDEXx.u.Ff.K > div > a")).map(x => x.textContent);
-    });
-    await fs.writeFile("Restaurants/names.txt", names.join("\r\n"))
+    const { browser, page } = await startBrowser();
+    const url = "https://fr.tripadvisor.be/Restaurants-g188646-Charleroi_Hainaut_Province_Wallonia.html";
+
+    await goToURL(page, url);
+    await captureScreenshot(page, "images/bg.png");
+
+    const names = await extractNames(page);
+    await writeToFile("Restaurants/info.json", names);
+
     await browser.close();
 }
 
